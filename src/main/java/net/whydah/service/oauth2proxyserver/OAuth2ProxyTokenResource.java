ae1df1111b784b62d8cdb94c3e4b8e62b720c2fb
totto <totto@totto.org>
2020-09-29 14:20:17 +0200

    Added some logging to try to figure out why client lookup fails for refresh_token

--- src/main/java/net/whydah/service/oauth2proxyserver/OAuth2ProxyTokenResource.java
+++ src/main/java/net/whydah/service/oauth2proxyserver/OAuth2ProxyTokenResource.java
@@ -46,21 +46,21 @@ public class OAuth2ProxyTokenResource {
 
     @GET
     public Response getOauth2ProxyServerController(
-    		@QueryParam("grant_type") String grant_type, 
-    		@QueryParam("client_id") String client_id, 
-    		@QueryParam("client_secret") String client_secret) throws MalformedURLException, AppException {
-        log.trace("getOAuth2ProxyServerController - /token got grant_type: {}",grant_type);
-        log.trace("getOAuth2ProxyServerController - /token got client_id: {}",client_id);
-        log.trace("getOAuth2ProxyServerController - /token got client_secret: {}",client_secret);
+            @QueryParam("grant_type") String grant_type,
+            @QueryParam("client_id") String client_id,
+            @QueryParam("client_secret") String client_secret) throws MalformedURLException, AppException {
+        log.trace("getOAuth2ProxyServerController - /token got grant_type: {}", grant_type);
+        log.trace("getOAuth2ProxyServerController - /token got client_id: {}", client_id);
+        log.trace("getOAuth2ProxyServerController - /token got client_secret: {}", client_secret);
 
-        if (credentialStore.hasWhydahConnection()){
+        if (credentialStore.hasWhydahConnection()) {
             log.trace("getOAuth2ProxyServerController - check STS");
             List<Application> applications = credentialStore.getWas().getApplicationList();
-            boolean found_clientId=false;
-            for (Application application:applications){
+            boolean found_clientId = false;
+            for (Application application : applications) {
                 if (application.getId().equalsIgnoreCase(ClientIDUtil.getApplicationId(client_id))) {
                     log.info("Valid applicationID found ");
-                    found_clientId=true;
+                    found_clientId = true;
                     // TODO - Call the STS and return
 
                 }
@@ -69,7 +69,7 @@ public class OAuth2ProxyTokenResource {
             if (!found_clientId) {
                 log.error("No clientId found");
 
-               throw AppExceptionCode.CLIENT_NOTFOUND_8002;
+                throw AppExceptionCode.CLIENT_NOTFOUND_8002;
             }
         }
         log.warn("getOAuth2ProxyServerController - no Whydah - dummy standalone fallback");
@@ -80,45 +80,47 @@ public class OAuth2ProxyTokenResource {
 
     /**
      * Expect Basic Authentication with client_id:client_secret
+     *
      * @param grant_type
      * @param code
      * @param scope
      * @param body
      * @param uriInfo
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
     @POST
     @Consumes("application/x-www-form-urlencoded")
     public Response buildTokenFromFormParameters(
-    		@FormParam("grant_type") String grant_type, 
-    		@FormParam("code") String code, 
-    		@FormParam("refresh_token") String refresh_token,
-    		@RequestBody String body,                                     
-    		@Context UriInfo uriInfo, 
-    		@Context HttpServletRequest request) throws Exception {
-
-        return build(grant_type, code, refresh_token, request);
-    }
-    
-    @POST
-    public Response buildToken(
-    		@QueryParam("grant_type") String grant_type, 
-    		@QueryParam("code") String code,
-            @QueryParam("refresh_token") String refresh_token, 
-            @RequestBody String body, 
+            @FormParam("grant_type") String grant_type,
+            @FormParam("code") String code,
+            @FormParam("refresh_token") String refresh_token,
+            @RequestBody String body,
+            @Context UriInfo uriInfo,
             @Context HttpServletRequest request) throws Exception {
 
         return build(grant_type, code, refresh_token, request);
     }
 
-    
+    @POST
+    public Response buildToken(
+            @QueryParam("grant_type") String grant_type,
+            @QueryParam("code") String code,
+            @QueryParam("refresh_token") String refresh_token,
+            @RequestBody String body,
+            @Context HttpServletRequest request) throws Exception {
 
-	private Response build(String grant_type, String code, String refresh_token, HttpServletRequest request) throws Exception {
-		Response response = null;
+        return build(grant_type, code, refresh_token, request);
+    }
+
+
+    private Response build(String grant_type, String code, String refresh_token, HttpServletRequest request) throws Exception {
+        Response response = null;
         String basicAuth = request.getHeader(ATHORIZATION);
         String client_id = findClientId(basicAuth);
+        log.info("clientId:" + client_id);
         String client_secret = findClientSecret(basicAuth);
+        log.info("client_secret:" + client_secret);
         if (clientService.isClientValid(client_id)) {
             String accessToken = buildAccessToken(client_id, client_secret, grant_type, code, refresh_token);
             if (accessToken == null) {
@@ -132,9 +134,8 @@ public class OAuth2ProxyTokenResource {
             throw AppExceptionCode.CLIENT_NOTFOUND_8002;
         }
         return response;
-	}
+    }
 
-   
 
     String findClientId(String basicAuth) {
         String clientId = null;
@@ -142,6 +143,7 @@ public class OAuth2ProxyTokenResource {
         if (credentials != null && credentials.length > 0) {
             clientId = credentials[0];
         }
+        log.info("Found clientId:" + clientId + " from basicAuth:" + basicAuth);
         return clientId;
     }
 
@@ -151,10 +153,11 @@ public class OAuth2ProxyTokenResource {
         if (credentials != null && credentials.length > 1) {
             clientSecret = credentials[1];
         }
+        log.info("Found clientSecret:" + clientSecret + " from basicAuth:" + basicAuth);
         return clientSecret;
     }
 
-    String[] findCredentials(String basicAuth ) {
+    String[] findCredentials(String basicAuth) {
         String[] values = null;
         if (basicAuth != null && basicAuth.startsWith("Basic")) {
             String base64Credentials = basicAuth.substring("Basic".length()).trim();
@@ -166,31 +169,30 @@ public class OAuth2ProxyTokenResource {
     }
 
 
-
     String buildAccessToken(String client_id, String client_secret, String grant_type, String theUsersAuthorizationCode, String refresh_token) throws Exception {
 
-        log.trace("oauth2ProxyServerController - /token got grant_type: {}",grant_type);
+        log.trace("oauth2ProxyServerController - /token got grant_type: {}", grant_type);
 
         String accessToken = null;
         boolean isClientIdValid = clientService.isClientValid(client_id);
         if (isClientIdValid) {
             accessToken = createAccessToken(client_id, client_secret, grant_type, theUsersAuthorizationCode, refresh_token);
         } else {
-        	throw AppExceptionCode.CLIENT_NOTFOUND_8002;
+            throw AppExceptionCode.CLIENT_NOTFOUND_8002;
         }
         log.warn("oauth2ProxyServerController - no Whydah - dummy standalone fallback");
         return accessToken;
     }
 
     protected String createAccessToken(String client_id, String client_secret, String grant_type, String theUsersAuthorizationCode, String refresh_token) throws Exception {
-      
-        String accessToken = null; 
-        if ("client_credentials".equalsIgnoreCase(grant_type)){
+
+        String accessToken = null;
+        if ("client_credentials".equalsIgnoreCase(grant_type)) {
             accessToken = "{ \"access_token\":\"" + ConstantValue.ATOKEN + "\" }";
-        } else if ("authorization_code".equalsIgnoreCase(grant_type)){
+        } else if ("authorization_code".equalsIgnoreCase(grant_type)) {
             accessToken = authorizationService.buildAccessToken(client_id, client_secret, theUsersAuthorizationCode);
-        } else if("refresh_token".equalsIgnoreCase(grant_type)) {
-        	accessToken = authorizationService.refreshAccessToken(client_id, client_secret, refresh_token);
+        } else if ("refresh_token".equalsIgnoreCase(grant_type)) {
+            accessToken = authorizationService.refreshAccessToken(client_id, client_secret, refresh_token);
         }
         return accessToken;
     }
