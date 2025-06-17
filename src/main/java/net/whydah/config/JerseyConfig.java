1007b90697de8cb0b4f79dc83d2cb247798750b3
totto <totto@totto.org>
2025-06-17 22:14:42 +0200

    a test

--- src/main/java/net/whydah/config/JerseyConfig.java
+++ src/main/java/net/whydah/config/JerseyConfig.java
@@ -1,5 +1,8 @@
 package net.whydah.config;
 
+import net.whydah.service.authorizations.UserAuthorizationService;
+import net.whydah.service.clients.ClientService;
+import net.whydah.service.oauth2proxyserver.TokenService;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.MvcFeature;
 import org.glassfish.jersey.server.spring.SpringLifecycleListener;
@@ -14,11 +17,20 @@ public class JerseyConfig extends ResourceConfig {
         register(SpringLifecycleListener.class);
         register(RequestContextFilter.class);
 
+        // CRITICAL: Enable Spring context injection FIRST
+        property("jersey.config.server.spring.context.inject", true);
+
+        // CRITICAL: Enable automatic Spring bean discovery
+        property("jersey.config.server.spring.context.scan", true);
+
         // Let Spring manage all resources via package scanning
-        //packages("net.whydah.service.oauth2proxyserver");
-        //packages("net.whydah.service.authorizations");
-        //packages("net.whydah.service.health");
         packages("net.whydah.service");
+
+        // Manual registration of key services (fallback if auto-discovery fails)
+        register(TokenService.class);
+        register(UserAuthorizationService.class);
+        register(ClientService.class);
+
 
         // Register filters and other providers
         register(net.whydah.service.CorsFilter.class);
@@ -27,10 +39,10 @@ public class JerseyConfig extends ResourceConfig {
         register(org.glassfish.jersey.server.mvc.freemarker.FreemarkerMvcFeature.class);
         property(MvcFeature.TEMPLATE_BASE_PATH, "templates");
 
-        // Enable Spring integration - Jersey will use Spring-managed instances
-        property("jersey.config.server.spring.context.inject", true);
-
         // Let Jersey discover Spring-managed resources
         property("jersey.config.server.provider.scanning.recursive", true);
+
+        // IMPORTANT: Make sure Jersey knows to look for Spring-managed beans
+        property("jersey.config.server.spring.scope.singleton", false);
     }
 }
